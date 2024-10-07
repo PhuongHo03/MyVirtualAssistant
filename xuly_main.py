84a9d8f2ebb8d927d21d0f781fe6b684a2ea654f
pMplayerI <cusstop386@gmail.com>
2024-10-07 18:08:43 +0700

    Add all

---
+++ xuly_main.py
@@ -0,0 +1,9 @@
+
+from main import Ui_MainWindow
+
+class Main(Ui_MainWindow):
+    # function
+        
+    def __init__(self,mainwindow)->None:
+        self.setupUi(mainwindow)
+    # =============================================
